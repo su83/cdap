c454e120219adb0b25dc6c56feb9079fdb8ea374
Patrick Guimarais <pdc269@nyu.edu>
2016-11-16 17:14:10 -0800

    adds warning symbol inside set provision btn

--- cdap-ui/app/cdap/api/serviceproviders/index.js
+++ cdap-ui/app/cdap/api/serviceproviders/index.js
@@ -24,5 +24,6 @@ export const MyServiceProviderApi = {
   list: apiCreator(dataSrc, 'GET', 'REQUEST', basepath),
   pollList: apiCreator(dataSrc, 'GET', 'POLL', basepath),
   get: apiCreator(dataSrc, 'GET', 'REQUEST', `${basepath}/:serviceprovider/stats`),
-  getServicesList: apiCreator(dataSrc, 'GET', 'POLL', '/system/services')
+  getServicesList: apiCreator(dataSrc, 'GET', 'POLL', '/system/services'),
+  setProvisions: apiCreator(dataSrc, 'PUT', 'REQUEST', '/system/services/:serviceid/instances')
 };
--- cdap-ui/app/cdap/components/ServiceStatus/index.js
+++ cdap-ui/app/cdap/components/ServiceStatus/index.js
@@ -20,43 +20,123 @@ var classNames = require('classnames');
 import Datasource from 'services/datasource';
 import {Dropdown, DropdownMenu} from 'reactstrap';
 import T from 'i18n-react';
-
+import {MyServiceProviderApi} from 'api/serviceproviders';
 export default class ServiceStatus extends Component {
 
   constructor(props){
     super(props);
-    this.toggleDropdown = this.toggleDropdown.bind(this);
+    this.toggleDropdownAndUpdate = this.toggleDropdownAndUpdate.bind(this);
     this.state = {
       isDropdownOpen : false,
       provisioned : this.props.numProvisioned,
       requested : this.props.numProvisioned,
-      editProvisioned : false,
-      serviceWarning  : false
+      editingProvisions : false,
+      serviceWarning  : false,
+      showError : false,
+      provisionsLoading : false,
+      enteredProvisionValue : '',
+      showBtnError : false
     };
     this.MyDataSrc = new Datasource();
-    this.editProvisions = this.editProvisions.bind(this);
+    this.keyDown = this.keyDown.bind(this);
+    this.toggleErrorMessage = this.toggleErrorMessage.bind(this);
+    this.onProvisionChange = this.onProvisionChange.bind(this);
+    this.toggleErrorBtn = this.toggleErrorBtn.bind(this);
   }
-  toggleDropdown(){
-    this.setState({
-      isDropdownOpen : !this.state.isDropdownOpen
-    });
-  }
-  editProvisions() {
-    this.setState({
-      editProvisoned : !this.state.editProvisoned
-    });
-  }
-  keyDown(e){
-    //If the enter key is pressed
-    if(e.keyCode !== undefined && e.keyCode === 13){
-      this.toggleDropdown();
+
+  //If the dropdown is open and the entered value is a number
+  toggleDropdownAndUpdate(){
+
+    if(this.state.isDropdownOpen && !isNaN(Number(this.state.enteredProvisionValue))){
+      //Make a request and set state to loading until we receive the response
+      this.setState({
+        isDropdownOpen : !this.state.isDropdownOpen,
+        serviceWarning : true
+      });
+
+      MyServiceProviderApi.setProvisions({
+        //Place the current name in the request
+        serviceid : this.props.name
+      }, {
+        instances : this.state.enteredProvisionValue
+      }).subscribe(() => {
+        this.setState({
+          provisionsLoading : false,
+          enteredProvisionValue : ''
+        });
+      });
+
+    } else {
+      //Toggle the dropdown menu
+      this.setState({
+        isDropdownOpen : !this.state.isDropdownOpen
+      });
     }
   }
+
+  onProvisionChange(e){
+    this.setState({
+      requested : e.target.value
+    });
+  }
+  //If the user presses enter, we set the state to reflect the entered value and close the dropdown
+  keyDown(e){
+    if(e.keyCode !== undefined && e.keyCode === 13){
+      //If there is no entered value do not set entered value
+      if(e.target.value === ''){
+        this.toggleDropdownAndUpdate();
+      } else {
+        this.setState({
+          enteredProvisionValue : e.target.value
+        }, () => {
+          this.toggleDropdownAndUpdate();
+        });
+      }
+    }
+  }
+
+  setProvisionNumber(){
+
+  }
+
+  //IF there is an error message, toggle depending on the event type
+  toggleErrorMessage(e){
+    if(this.state.serviceWarning){
+      if(e.type === 'mouseover'){
+        this.setState({
+          showError : true
+        });
+      } else if(e.type === 'mouseout'){
+        this.setState({
+          showError : false
+        });
+      }
+    }
+  }
+  //IF there is an error message, toggle depending on the event type
+  toggleErrorBtn(e){
+    if(this.state.serviceWarning){
+      if(e.type === 'mouseover'){
+        this.setState({
+          showBtnError : true
+        });
+      } else if(e.type === 'mouseout'){
+        this.setState({
+          showBtnError : false
+        });
+      }
+    }
+  }
+
   render(){
     var circle = '';
 
     if(!this.props.isLoading){
-      if(this.props.status === 'OK'){
+      if(this.state.provisionsLoading){
+        circle = <div className={classNames({"status-circle-green" : !this.props.isLoading, "status-circle-grey" : this.props.isLoading})}>
+                  <span className="fa fa-cog fa-spin fa-fw" />
+                 </div>;
+      } else if(this.props.status === 'OK'){
         circle = <div className={classNames({"status-circle-green" : !this.props.isLoading, "status-circle-grey" : this.props.isLoading})}>
                   {this.state.provisioned}
                  </div>;
@@ -71,21 +151,41 @@ export default class ServiceStatus extends Component {
       _cdapPath : `/system/services/${this.props.name}/logs/next?&maxSize=50`
     });
 
+    let provisionBtnClasses = classNames('btn btn-default set-provision-btn', {'provision-btn-with-warning' : this.state.serviceWarning});
+
     return (
-      <div onClick={this.toggleDropdown} className="service-status">
+      <div
+        onClick={this.toggleDropdownAndUpdate}
+        className="service-status"
+        onMouseOver={this.toggleErrorMessage}
+        onMouseOut={this.toggleErrorMessage}
+      >
         {circle}
         <div className="status-label">
           {T.translate(`features.Management.Services.${this.props.name.split('.').join('_')}`)}
           {
             this.state.serviceWarning ?
-              <i className="fa fa-exclamation-triangle" aria-hidden="true"></i>  :
+              <span
+                className="fa fa-exclamation-triangle"
+                aria-hidden="true"
+              >
+              {
+                this.state.showError && !this.state.isDropdownOpen?
+                <div className="service-error-message">
+                  Instance count should be between [1,1]
+                </div>
+                :
+                null
+              }
+              </span>
+              :
               null
           }
         </div>
         <div className="service-dropdown-container pull-right">
           <Dropdown
             isOpen={this.state.isDropdownOpen}
-            toggle={this.toggleDropdown}
+            toggle={this.toggleDropdownAndUpdate}
             className="service-dropdown"
           >
             <span className="fa fa-caret-down service-dropdown-caret">
@@ -103,22 +203,46 @@ export default class ServiceStatus extends Component {
                     View Logs
                 </div>
               </a>
-              {
-                this.state.editProvisioned ?
-                  <div onClick={this.editProvisions} className="provision-dropdown-item service-dropdown-item">
-                    Provisions
-                    <input
-                      className="provision-input"
-                      placeholder={this.state.provisioned}
-                      onChange={this.updateProvisioned}
-                      onKeyDown={this.keyDown}
-                    />
-                  </div>
-                  :
-                  <div onClick={this.editProvisions} className="provision-dropdown-item service-dropdown-item">
-                    Provisioned {this.state.provisioned}
-                  </div>
-              }
+              <div
+                className="provision-dropdown-item service-dropdown-item"
+                onMouseOver={this.toggleErrorMessage}
+                onMouseOut={this.toggleErrorMessage}
+              >
+                Provisions
+                <input
+                  className="provision-input"
+                  autoFocus
+                  placeholder={this.state.provisioned}
+                  onChange={this.onProvisionChange}
+                  onKeyDown={this.keyDown}
+                />
+                <button
+                  onMouseOver={this.toggleErrorBtn}
+                  onMouseOut={this.toggleErrorBtn}
+                  className={provisionBtnClasses}
+                  onClick={this.setProvisionNumber}
+                >
+                  {T.translate(`features.Management.Services.setBtn`)}
+                  {
+                    this.state.serviceWarning ?
+                      <span
+                        className="warning-triangle fa fa-exclamation-triangle"
+                        aria-hidden="true"
+                      >
+                      {
+                        this.state.showBtnError && this.state.isDropdownOpen ?
+                        <div className="service-error-message-inline">
+                          Instance count should be between [1,1]
+                        </div>
+                        :
+                        null
+                      }
+                      </span>
+                      :
+                      null
+                  }
+                </button>
+              </div>
             </DropdownMenu>
           </Dropdown>
         </div>
